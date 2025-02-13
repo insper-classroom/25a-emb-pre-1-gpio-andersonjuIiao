58f7af5acf614b54f81b85661bfec47df0646f10
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-02-12 22:06:37 -0300

    try 4

--- exe5/main.c
+++ exe5/main.c
@@ -21,11 +21,19 @@ int main() {
     while (true) {
 
         if (!gpio_get(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
+            sleep_ms(138);
+            cnt_1++;
+            printf("Botao 1: %d\n", cnt_1);
+            while (!gpio_get(BTN_PIN)) {
+            };
         }
 
         if (!gpio_get(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
+            sleep_ms(69);
+            cnt_2++;
+            printf("Botao 2: %d\n", cnt_2);
+            while (!gpio_get(BTN_PIN_2)) {
+            };
         }
     }
 }
