09773774e72d782a3f52c71cec8051918189b227
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-02-12 21:28:09 -0300

    try 3

--- exe3/main.c
+++ exe3/main.c
@@ -10,9 +10,6 @@ const int BTN_PIN_G = 26;
 int main() {
   stdio_init_all();
 
-  int valor_G;
-  int valor_R;
-
   gpio_init(LED_PIN_R);
   gpio_init(LED_PIN_G);
   gpio_set_dir(LED_PIN_R, GPIO_OUT);
@@ -25,23 +22,27 @@ int main() {
   gpio_pull_up(BTN_PIN_G);
   gpio_pull_up(BTN_PIN_R);
 
+  int valor_G = 0;
+  int valor_R = 0;
+
   while (true) {
 
-    valor_G = gpio_get(BTN_PIN_G);
-    valor_R = gpio_get(BTN_PIN_R);
-
     if (!gpio_get(BTN_PIN_G)) {
-      gpio_put(LED_PIN_G, !valor_G);
-      printf("Verde");
-      while (!gpio_get(BTN_PIN_G)) {
-      };
+      valor_G = !valor_G;
+      gpio_put(LED_PIN_G, valor_G);
+      sleep_ms(100);
+      //printf("Verde");
+      //while (!gpio_get(BTN_PIN_G)) {
+      //};
     }
 
     if (!gpio_get(BTN_PIN_R)) {
+      valor_R = !valor_R;
       gpio_put(LED_PIN_R, !valor_R);
-      printf("Red");
-      while (!gpio_get(BTN_PIN_R)) {
-      };
+      sleep_ms(100);
+      //printf("Red");
+      //while (!gpio_get(BTN_PIN_R)) {
+      //};
     }
   }
 }
