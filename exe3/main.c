14383c78135d78cebfc73dbe9a57eb38b00be158
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-03-29 16:22:05 -0300

    estudo PI - Ex.03

--- exe3/main.c
+++ exe3/main.c
@@ -1,10 +1,46 @@
 #include "hardware/gpio.h"
  #include "pico/stdlib.h"
  #include <stdio.h>
- 
+
+ const int LED_PIN_R = 4;
+ const int LED_PIN_G = 6;
+
+ const int BTN_PIN_R = 28;
+ const int BTN_PIN_G = 26;  
+
+ int CTR_R = 0;
+ int CTR_G = 0;
+
  int main() {
    stdio_init_all();
+
+   gpio_init(LED_PIN_R);
+   gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+   gpio_init(LED_PIN_G);
+   gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+   gpio_init(BTN_PIN_R);
+   gpio_set_dir(BTN_PIN_R, GPIO_IN);
+   gpio_pull_up(BTN_PIN_R);
+
+   gpio_init(BTN_PIN_G);
+   gpio_set_dir(BTN_PIN_G, GPIO_IN);
+   gpio_pull_up(BTN_PIN_G);
  
    while (true) {
+    if (!gpio_get(BTN_PIN_R)) {
+      CTR_R = !CTR_R;
+      gpio_put(LED_PIN_R, !CTR_R);
+      while (!gpio_get(BTN_PIN_R)) {
+      };
+    }
+    if (!gpio_get(BTN_PIN_G)) {
+      CTR_G = !CTR_G;
+      gpio_put(LED_PIN_G, CTR_G);
+      while (!gpio_get(BTN_PIN_G)) {
+      };
+    }
+
    }
  }
