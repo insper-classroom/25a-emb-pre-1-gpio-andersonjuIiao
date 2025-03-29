0830a392a1122b4af82ef61baf1fa87f5d62e3ee
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-03-29 15:55:20 -0300

    estudo PI - Ex.01

--- exe1/main.c
+++ exe1/main.c
@@ -1,25 +1,26 @@
 #include "hardware/gpio.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
+ #include "pico/stdlib.h"
+ #include <stdio.h>
+ 
+ const int LED_PIN_R = 5;
+ const int LED_PIN_Y = 28;
+ 
+ int main() {
+   stdio_init_all();
+ 
+   gpio_init(LED_PIN_R);
+   gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
-const int LED_PIN_R = 5;
-const int LED_PIN_Y = 28;
-
-int main() {
-  stdio_init_all();
-
-  gpio_init(LED_PIN_R);
-  gpio_init(LED_PIN_Y);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
-  gpio_set_dir(LED_PIN_Y, GPIO_OUT);
-
-  while (true) {
-    gpio_put(LED_PIN_R, 1);
-    gpio_put(LED_PIN_Y, 1);
-    sleep_ms(250);
-
-    gpio_put(LED_PIN_R, 0);
-    gpio_put(LED_PIN_Y, 0);
-    sleep_ms(250);
-  }
-}
+   gpio_init(LED_PIN_Y);
+   gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+ 
+   while (true) {
+     gpio_put(LED_PIN_R, 1);
+     gpio_put(LED_PIN_Y, 1);
+     sleep_ms(250);
+ 
+     gpio_put(LED_PIN_R, 0);
+     gpio_put(LED_PIN_Y, 0);
+     sleep_ms(250);
+   }
+ }
--- exe3/main.c
+++ exe3/main.c
@@ -1,44 +1,10 @@
 #include "hardware/gpio.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
-
-const int LED_PIN_R = 4;
-const int LED_PIN_G = 6;
-const int BTN_PIN_R = 28;
-const int BTN_PIN_G = 26;
-
-int main() {
-  stdio_init_all();
-
-  gpio_init(LED_PIN_R);
-  gpio_init(LED_PIN_G);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
-  gpio_set_dir(LED_PIN_G, GPIO_OUT);
-
-  gpio_init(BTN_PIN_G);
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_G, GPIO_IN);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_G);
-  gpio_pull_up(BTN_PIN_R);
-
-  int valor_G;
-  int valor_R;
-
-  while (true) {
-
-    if (!gpio_get(BTN_PIN_G)) {
-      valor_G = gpio_get(LED_PIN_G);
-      gpio_put(LED_PIN_G, !valor_G);
-      while (!gpio_get(BTN_PIN_G)) {
-      };
-    }
-
-    if (!gpio_get(BTN_PIN_R)) {
-      valor_R = gpio_get(LED_PIN_R);
-      gpio_put(LED_PIN_R, !valor_R);
-      while (!gpio_get(BTN_PIN_R)) {
-      };
-    }
-  }
-}
+ #include "pico/stdlib.h"
+ #include <stdio.h>
+ 
+ int main() {
+   stdio_init_all();
+ 
+   while (true) {
+   }
+ }
