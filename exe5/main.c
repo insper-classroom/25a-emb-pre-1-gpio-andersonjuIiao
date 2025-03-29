c3eac7b4424efac781e343c55e4712246e53d944
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-03-29 16:54:57 -0300

    estudo PI - Ex.05

--- exe5/main.c
+++ exe5/main.c
@@ -15,16 +15,19 @@
      gpio_init(BTN_PIN_2);
      gpio_set_dir(BTN_PIN_2, GPIO_IN);
      gpio_pull_up(BTN_PIN_2);
+
      int cnt_1 = 0;
      int cnt_2 = 0;
  
      while (true) {
  
          if (!gpio_get(BTN_PIN)) {
+             sleep_ms(500);
              printf("Botao 1: %d\n", cnt_1++);
          }
  
          if (!gpio_get(BTN_PIN_2)) {
+             sleep_ms(500);
              printf("Botao 2: %d\n", cnt_2++);
          }
      }
